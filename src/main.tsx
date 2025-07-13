9c1b87971e0e4631260161c6d2368cd9d92481ab
0hm☘️ <git8ankanroy@gmail.com>
2025-07-13 17:39:39 +0530

    fix: skeleton not added

--- src/main.tsx
+++ src/main.tsx
@@ -49,7 +49,7 @@ createRoot(document.getElementById('root')!).render(
                 <Suspense
                   fallback={
                     <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-700 text-2xl">
-                      Loading Blog Post...
+                      <SkeletonBlogCard />
                     </div>
                   }
                 >
@@ -63,7 +63,7 @@ createRoot(document.getElementById('root')!).render(
                 <Suspense
                   fallback={
                     <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-700 text-2xl">
-                      Loading Contact Page...
+                      <LazyBlogPostDetail />
                     </div>
                   }
                 >
